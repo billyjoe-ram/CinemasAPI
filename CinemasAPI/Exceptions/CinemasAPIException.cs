cdc133d5d7867c5bdd543af880d71457854d6492
Billy Joe <billyjoersantos@outlook.com>
2022-04-02 08:58:18 -0300

    Adicionando Exceptions customizadas para utilizar na aplicação

---
+++ CinemasAPI/Exceptions/CinemasAPIException.cs
@@ -0,0 +1,18 @@
+﻿namespace CinemasAPI.Exceptions
+{
+    public class CinemasAPIException : Exception
+    {
+        public CinemasAPIException() : base()
+        {
+        }
+
+        public CinemasAPIException(string? message) : base(message)
+        {
+        }
+
+        public CinemasAPIException(string? message, Exception? innerException) :
+            base(message, innerException)
+        {
+        }
+    }
+}
---
+++ CinemasAPI/Exceptions/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace CinemasAPI.Exceptions
+{
+    public class NotFoundException : CinemasAPIException
+    {
+        public NotFoundException() : base()
+        {
+        }
+
+        public NotFoundException(string? message) : base(message)
+        {
+        }
+
+        public NotFoundException(string? message, Exception? innerException) :
+            base(message, innerException)
+        {
+        }
+    }
+}
