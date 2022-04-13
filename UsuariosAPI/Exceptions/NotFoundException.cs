3a8f008e7b9f2b30d558636bea337a2c3c8e02f3
Billy Joe <billyjoersantos@outlook.com>
2022-04-13 05:17:09 -0300

    Criando uma exceção para caso usuario não tenha sido encontrado

---
+++ UsuariosAPI/Exceptions/NotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace UsuariosAPI.Exceptions
+{
+    public class NotFoundException : UsuariosAPIException
+    {
+        public NotFoundException() : base()
+        {
+        }
+
+        public NotFoundException(string? message) : base(message)
+        {
+        }
+
+        public NotFoundException(string? message, Exception? innerException) :
+            base(message, innerException)
+        {
+        }
+    }
+}
