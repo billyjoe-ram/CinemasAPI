753df1ba8cbe7727ec277bf1a1f726adfd69e4cd
Billy Joe <billyjoersantos@outlook.com>
2022-04-17 16:04:17 -0300

    Documentando controllers de Cinemas

--- CinemasAPI/Controllers/CinemasController.cs
+++ CinemasAPI/Controllers/CinemasController.cs
@@ -7,17 +7,29 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace CinemasAPI.Controllers
 {
+    /// <summary>
+    ///     Controller para as operações relacionadas aos cinemas
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class CinemasController : ControllerBase
     {
-        private CinemasService _cinemasService;
+    private CinemasService _cinemasService;
 
+        /// <summary>
+        ///     Inicia uma nova instância da classe <see cref="CinemasController"/> .
+        /// </summary>
+        /// <param name="cinemasService"></param>
         public CinemasController(CinemasService cinemasService)
         {
             _cinemasService = cinemasService;
         }
 
+        /// <summary>
+        ///     Adiciona um Cinema aos registros.
+        /// </summary>
+        /// <param name="cinemaDto">Representa o DTO para criação do Cinema.</param>
+        /// <returns>Resultado de ação realizada.</returns>
         [HttpPost]
         [Authorize(Roles = "admin")]
         public IActionResult AddCinema([FromBody] CreateCinemaDto cinemaDto)
@@ -31,6 +43,11 @@ namespace CinemasAPI.Controllers
             );
         }
 
+        /// <summary>
+        ///     Busca um cinema no banco de dados com o id passado.
+        /// </summary>
+        /// <param name="idCinema">O id respectivo do Cinema no banco de dados.</param>
+        /// <returns>Resultado da ação, com o Cinema em JSON.</returns>
         [HttpGet("{idCinema}")]
         [Authorize(Roles = "admin, user")]
         public ActionResult<ReadCinemaDto> FetchCinema(int idCinema)
@@ -49,6 +66,12 @@ namespace CinemasAPI.Controllers
             return Ok(cinema);
         }
 
+        /// <summary>
+        ///     Atualiza um cinema no banco de dados com o id passado.
+        /// </summary>
+        /// <param name="idCinema">O id respectivo do Cinema no banco de dados.</param>
+        /// <param name="cinemaNovoDto">O novo objeto, com os dados a serem alterados.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpPut("{idCinema}")]
         [Authorize(Roles = "admin")]
         public IActionResult UpdateCinema(int idCinema, [FromBody] UpdateCinemaDto cinemaNovoDto)
@@ -65,6 +88,11 @@ namespace CinemasAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        ///     Exclue um registro de cinema no banco de dados.
+        /// </summary>
+        /// <param name="idCinema">O id respectivo do Cinema no banco de dados.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpDelete("{idCinema}")]
         [Authorize(Roles = "admin")]
         public IActionResult DeleteCinema(int idCinema)
