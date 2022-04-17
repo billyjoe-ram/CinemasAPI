be1c29a445ba14db8e15c94dcf28c33dae42a6d3
Billy Joe <billyjoersantos@outlook.com>
2022-04-17 16:16:12 -0300

    Documentando controller de Enderecos

--- CinemasAPI/Controllers/EnderecosController.cs
+++ CinemasAPI/Controllers/EnderecosController.cs
@@ -7,17 +7,30 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace CinemasAPI.Controllers
 {
+
+    /// <summary>
+    ///     Controller para as operações relacionadas a endereços
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class EnderecosController : ControllerBase
     {
         private EnderecosService _enderecosService;
 
+        /// <summary>
+        ///     Inicia uma nova instância da classe <see cref="EnderecosController"/> .
+        /// </summary>
+        /// <param name="enderecosService"></param>
         public EnderecosController(EnderecosService enderecosService)
         {
             _enderecosService = enderecosService;
         }
 
+        /// <summary>
+        ///     Adiciona um Endereço aos registros.
+        /// </summary>
+        /// <param name="enderecoDto">Representa o DTO para criação de Endereco.</param>
+        /// <returns>Resultado de ação realizada.</returns>
         [HttpPost]
         [Authorize(Roles = "admin")]
         public IActionResult AddEndereco([FromBody] CreateEnderecoDto enderecoDto)
@@ -31,6 +44,10 @@ namespace CinemasAPI.Controllers
             );
         }
 
+        /// <summary>
+        ///     Busca todo os Endereços no banco de dados.
+        /// </summary>
+        /// <returns>Resultado da ação, com os Endereços em JSON.</returns>
         [HttpGet]
         [Authorize(Roles = "admin, user")]
         public ActionResult<IEnumerable<ReadEnderecoDto>> FetchEnderecos()
@@ -49,6 +66,11 @@ namespace CinemasAPI.Controllers
             return Ok(enderecos);
         }
 
+        /// <summary>
+        ///     Busca um Endereço no banco de dados com o id passado.
+        /// </summary>
+        /// <param name="idEndereco">O id respectivo do Endereço no banco de dados.</param>
+        /// <returns>Resultado da ação, com o Endereço em JSON.</returns>
         [HttpGet("{idEndereco}")]
         public ActionResult<ReadEnderecoDto> FetchEndereco(int idEndereco)
         {
@@ -66,6 +88,12 @@ namespace CinemasAPI.Controllers
             return Ok(endereco);
         }
 
+        /// <summary>
+        ///     Atualiza um Endereço no banco de dados com o id passado.
+        /// </summary>
+        /// <param name="idEndereco">O id respectivo do Endereço a ser atualizado.</param>
+        /// <param name="enderecoNovoDto">O novo objeto, com os dados a serem alterados.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpPut("{idEndereco}")]
         [Authorize(Roles = "admin")]
         public IActionResult UpdateEndereco(int idEndereco, [FromBody] UpdateEnderecoDto enderecoNovoDto)
@@ -82,6 +110,11 @@ namespace CinemasAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        ///     Exclue um registro de Endereço no banco de dados.
+        /// </summary>
+        /// <param name="idEndereco">O id do Endereço a ser excluído.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpDelete("{idEndereco}")]
         [Authorize(Roles = "admin")]
         public IActionResult DeleteEndereco(int idEndereco)
