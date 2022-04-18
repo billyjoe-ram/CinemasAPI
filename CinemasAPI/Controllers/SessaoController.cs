2b4c2af9ed3eddf20158aefc390b7137c43b9c60
Billy Joe <billyjoersantos@outlook.com>
2022-04-18 07:21:16 -0300

    Documentando controller de Sessoes

--- CinemasAPI/Controllers/SessaoController.cs
+++ CinemasAPI/Controllers/SessaoController.cs
@@ -7,17 +7,29 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace CinemasAPI.Controllers
 {
+    /// <summary>
+    ///     Controller para as ações relacionadas a Sessões
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class SessaoController : ControllerBase
     {
         private SessaoService _sessaoService;
 
+        /// <summary>
+        ///     Inicia uma nova instância da classe <see cref="SessaoController"/>
+        /// </summary>
+        /// <param name="sessaoService"></param>
         public SessaoController(SessaoService sessaoService)
         {
             _sessaoService = sessaoService;
         }
 
+        /// <summary>
+        ///     Adiciona uma Sessão aos registros.
+        /// </summary>
+        /// <param name="sessaoDto">Representa o DTO para a criação de uma Sessão.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpPost]
         [Authorize(Roles = "admin")]
         public IActionResult AddSessao([FromBody] CreateSessaoDto sessaoDto)
@@ -31,6 +43,10 @@ namespace CinemasAPI.Controllers
             );
         }
 
+        /// <summary>
+        ///     Busca todas as Sessões no banco de dados.
+        /// </summary>
+        /// <returns>Resultado da ação, com as Sessões em JSON.</returns>
         [HttpGet]
         [Authorize(Roles = "admin, user")]
         public ActionResult<IEnumerable<ReadSessaoDto>> FetchSessoes()
@@ -49,6 +65,11 @@ namespace CinemasAPI.Controllers
             return Ok(sessoes);
         }
 
+        /// <summary>
+        ///     Busca uma Sessão no banco de dados com o id passado.
+        /// </summary>
+        /// <param name="idSessao">O id respectivo da Sessão no banco de dados.</param>
+        /// <returns> Resultado da ação, com a Sessão em JSON.</returns>
         [HttpGet("{idSessao}")]
         [Authorize(Roles = "admin, user")]
         public ActionResult<ReadSessaoDto> FetchSessao(int idSessao)
@@ -67,6 +88,12 @@ namespace CinemasAPI.Controllers
             return Ok(sessao);
         }
 
+        /// <summary>
+        ///     Atualiza uma Sessão no banco de dados com o id passado.
+        /// </summary>
+        /// <param name="idSessao">O id respectivo da Sessão a ser atualizada.</param>
+        /// <param name="sessaoNovaDto">O novo objeto, com os dados a serem alterados.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpPut("{idSessao}")]
         [Authorize(Roles = "admin")]
         public IActionResult UpdateSessao(int idSessao, [FromBody] UpdateSessaoDto sessaoNovaDto)
@@ -83,6 +110,11 @@ namespace CinemasAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        ///     Exclue um registro de Sessão no banco de dados.
+        /// </summary>
+        /// <param name="idSessao">O id da Sessão a ser excluída.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpDelete("{idSessao}")]
         [Authorize(Roles = "admin")]
         public IActionResult DeleteSessao(int idSessao)
