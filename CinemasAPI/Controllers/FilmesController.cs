34f25b3be007fc91c5d266208ab6cc670eb78597
Billy Joe <billyjoersantos@outlook.com>
2022-04-18 06:04:43 -0300

    Documentando controller de Filmes

--- CinemasAPI/Controllers/FilmesController.cs
+++ CinemasAPI/Controllers/FilmesController.cs
@@ -7,17 +7,29 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace CinemasAPI.Controllers
 {
+    /// <summary>
+    ///     Controller para as operações relacionadas a endereços
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class FilmesController : ControllerBase
     {
         private FilmesService _filmesService;
 
+        /// <summary>
+        ///     Inicia uma nova instância da classe <see cref="EnderecosController"/>.
+        /// </summary>
+        /// <param name="filmesService"></param>
         public FilmesController(FilmesService filmesService)
         {
             _filmesService = filmesService;
         }
 
+        /// <summary>
+        ///     Adiciona um Filme aos registros.
+        /// </summary>
+        /// <param name="filmeDto">Representa o DTO para criação de Filme.</param>
+        /// <returns>Resultado de ação realizada.</returns>
         [HttpPost]
         [Authorize(Roles = "admin")]
         public ActionResult AddFilme([FromBody] CreateFilmeDto filmeDto)
@@ -31,6 +43,12 @@ namespace CinemasAPI.Controllers
             );
         }
 
+        /// <summary>
+        ///     Busca todos os Filmes no banco de dados.
+        /// </summary>
+        /// <remarks>É possível filtrar os registros por meio da classificação etária.</remarks>
+        /// <param name="classificacaoEtaria"> Idade para filtrar classificação etária.ss</param>
+        /// <returns>Resultado da ação, com os Filmes em JSON.</returns>
         [HttpGet]
         [Authorize(Roles = "admin, user")]
         public ActionResult<IEnumerable<ReadFilmeDto>> FetchFilmes([FromQuery] int classificacaoEtaria = 18)
@@ -49,6 +67,11 @@ namespace CinemasAPI.Controllers
             return Ok(filmes);
         }
 
+        /// <summary>
+        ///     Busca um Filme no banco de dados com o id passado.
+        /// </summary>
+        /// <param name="idFilme">O id respectivo do Filme no banco de dados.</param>
+        /// <returns>Resultado da ação, com o Filme em JSON.</returns>
         [HttpGet("{idFilme}")]
         [Authorize(Roles = "admin, user")]
         public ActionResult<ReadFilmeDto> FetchFilme(int idFilme)
@@ -67,6 +90,12 @@ namespace CinemasAPI.Controllers
             return Ok(filme);
         }
 
+        /// <summary>
+        ///     Atualiza um Filme no banco de dados com o id passado.
+        /// </summary>
+        /// <param name="idFilme">O id respectivo do filme a ser atualizado.</param>
+        /// <param name="filmeNovoDto">O novo objeto, com os dados a serem alterados.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpPut("{idFilme}")]
         [Authorize(Roles = "admin")]
         public IActionResult UpdateFilme(int idFilme, [FromBody] UpdateFilmeDto filmeNovoDto)
@@ -83,6 +112,11 @@ namespace CinemasAPI.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        ///     Exclue um registro de Filme no banco de dados
+        /// </summary>
+        /// <param name="idFilme">O id do filme a ser excluído.</param>
+        /// <returns>Resultado a ação reallizada.</returns>
         [HttpDelete("{idFilme}")]
         [Authorize(Roles = "admin")]
         public IActionResult DeleteFilme(int idFilme)
