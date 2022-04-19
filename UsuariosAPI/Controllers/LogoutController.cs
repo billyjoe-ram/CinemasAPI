d9956f3942bc27973e4ae35f7979acf30fe85d3f
Billy Joe <billyjoersantos@outlook.com>
2022-04-19 06:48:22 -0300

    Documentando controller de Logout

--- UsuariosAPI/Controllers/LogoutController.cs
+++ UsuariosAPI/Controllers/LogoutController.cs
@@ -4,17 +4,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace UsuariosAPI.Controllers
 {
+    /// <summary>
+    ///     Controller para as ações relacionadas a logout
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class LogoutController : ControllerBase
     {
         private LogoutService _logoutService;
 
+        /// <summary>
+        ///     Inicia uma nova instância da classe <see cref="LogoutController"/>
+        /// </summary>
+        /// <param name="logoutService"></param>
         public LogoutController(LogoutService logoutService)
         {
             _logoutService = logoutService;
         }
 
+        /// <summary>
+        ///     Desconecta um usuário do sistema.
+        /// </summary>
+        /// <returns>Resultado da ação realizada.</returns>
         public IActionResult LogoutUsuario()
         {
             try
