f47fcff90c1e883d440dd33014121d6fd060ef29
Billy Joe <billyjoersantos@outlook.com>
2022-04-19 06:59:05 -0300

    Documentando controller de Usuarios

--- UsuariosAPI/Controllers/UsuariosController.cs
+++ UsuariosAPI/Controllers/UsuariosController.cs
@@ -6,16 +6,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace UsuariosAPI.Controllers
 {
+    /// <summary>
+    ///     Controller para as ações relacionadas ao usuário.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class UsuariosController : ControllerBase
     {
         private UsuariosService _usuariosService;
+
+        /// <summary>
+        ///     Inicia uma nova instância da classe <see cref="UsuariosController"/>
+        /// </summary>
+        /// <param name="usuariosService"></param>
         public UsuariosController(UsuariosService usuariosService)
         {
             _usuariosService = usuariosService;
         }
 
+        /// <summary>
+        ///     Cadastra um usuário no sistema.
+        /// </summary>
+        /// <param name="usuarioDto">Classe de Request para cadastrar o usuário.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpPost]
         public IActionResult CadastrarUsuario(CreateUsuarioDto usuarioDto)
         {
@@ -32,6 +45,14 @@ namespace UsuariosAPI.Controllers
             return Ok(new { CodigoAtivacao = codigoAtivacao });
         }
 
+        /// <summary>
+        ///     Ativa o usuário.
+        /// </summary>
+        /// <remarks>
+        ///     Utiliza o token ao cadastrar para ativação gerado.
+        /// </remarks>
+        /// <param name="ativaContaRequest">Classe de Request para ativar a conta.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpGet]
         [Route("/[controller]/ativar")]
         public IActionResult AtivarUsuario([FromQuery] AtivaContaRequest ativaContaRequest)
@@ -47,6 +68,14 @@ namespace UsuariosAPI.Controllers
             return Ok(new { Mensagem = "Conta ativada com sucesso"});
         }
 
+        /// <summary>
+        ///     Solicita a redefinição da senha.
+        /// </summary>
+        /// <remarks>
+        ///     Para o usuário enviado, será gerado um Token, necessário para redefinição.
+        /// </remarks>
+        /// <param name="request">Classe de Request para solicitar reset de senha.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpPost]
         [Route("/[controller]/solicitar-reset-senha")]
         public IActionResult SolicitarResetSenha([FromBody] SolicitacaoResetSenhaRequest request)
@@ -65,6 +94,11 @@ namespace UsuariosAPI.Controllers
             return Ok(new { Token = tokenResetSenha });
         }
 
+        /// <summary>
+        ///     Redefine a senha do usuário.
+        /// </summary>
+        /// <param name="request">Classe de Request para redefir senha.</param>
+        /// <returns>Resultado da ação realizada.</returns>
         [HttpPost]
         [Route("/[controller]/reset-senha")]
         public IActionResult ResetarSenha([FromBody] ResetSenhaRequest request)
