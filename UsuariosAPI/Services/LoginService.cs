8ae770e8cd563ef64b3e70fb1af5b8573efced77
Billy Joe <billyjoersantos@outlook.com>
2022-04-20 22:31:02 -0300

    Documentando service de Login

--- UsuariosAPI/Services/LoginService.cs
+++ UsuariosAPI/Services/LoginService.cs
@@ -5,17 +5,33 @@ using Microsoft.AspNetCore.Identity;
 
 namespace UsuariosAPI.Services
 {
+    /// <summary>
+    ///     Service para as operações e regras de negócio relacionado ao login.
+    /// </summary>
     public class LoginService
     {
         private SignInManager<IdentityUser<int>> _signInManager;
         private TokenService _tokenService;
 
+        /// <summary>
+        ///     Inicia uma nova instância da classe <see cref="LoginService"/>.
+        /// </summary>
+        /// <param name="signInManager"></param>
+        /// <param name="tokenService"></param>
         public LoginService(SignInManager<IdentityUser<int>> signInManager, TokenService tokenService)
         {
             _signInManager = signInManager;
             _tokenService = tokenService;
         }
 
+        /// <summary>
+        ///     Loga um usuário no sistema.
+        /// </summary>
+        /// <param name="request">Classe de Request para efetuar o login.</param>
+        /// <returns>O token de autenticação após realizar o login.</returns>
+        /// <exception cref="SignInException">
+        ///     Lançada quando não é possível realizar o login.
+        /// </exception>
         public string LogarUsuario(LoginRequest request)
         {           
             var identityUser = _signInManager
