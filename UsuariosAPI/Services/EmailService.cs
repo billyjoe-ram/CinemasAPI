088ff909eda99b2768d0beb917f754568e155f23
Billy Joe <billyjoersantos@outlook.com>
2022-04-21 15:30:03 -0300

    refactor(acessores/classes): UsuariosAPI - Service de E-mails
    Modifica o acessor de public para internal na classe EmailService
    
    Relacionado à Issue #1

--- UsuariosAPI/Services/EmailService.cs
+++ UsuariosAPI/Services/EmailService.cs
@@ -9,7 +9,7 @@ namespace UsuariosAPI.Services
     ///     Service para as operações e regras de negócio relacionadas aos
     ///     e-mails da UsuariosAPI
     /// </summary>
-    public class EmailService
+    internal class EmailService
     {
         private IConfiguration _configuration;
 
