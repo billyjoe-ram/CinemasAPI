deeb4d5d1ce693db0455bf2f3f63b77f620b96e2
Billy Joe <billyjoersantos@outlook.com>
2022-04-20 22:20:42 -0300

    Documentando service de Email

--- UsuariosAPI/Services/EmailService.cs
+++ UsuariosAPI/Services/EmailService.cs
@@ -5,15 +5,32 @@ using UsuariosAPI.Models;
 
 namespace UsuariosAPI.Services
 {
+    /// <summary>
+    ///     Service para as operações e regras de negócio relacionadas aos
+    ///     e-mails da UsuariosAPI
+    /// </summary>
     public class EmailService
     {
         private IConfiguration _configuration;
 
+        /// <summary>
+        ///     Inicia uma nova instância da classe <see cref="EmailService"/>
+        /// </summary>
+        /// <param name="configuration"></param>
         public EmailService(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
+        /// <summary>
+        ///     Envia um e-mail para a lista de destinatários informados.
+        /// </summary>
+        /// <remarks>
+        ///     Utiliza o remetente padrão da API para enviar o e-mail.
+        /// </remarks>
+        /// <param name="destinatarios">Lista de e-mails aos quais a mensagem será enviada.</param>
+        /// <param name="assunto">Assunto da mensagem de e-mail.</param>
+        /// <param name="corpoEmail">Corpo do texto do e-mail.</param>
         public void EnviarEmail(List<string> destinatarios, string assunto, string corpoEmail)
         {
             var email = new Email(destinatarios, assunto, corpoEmail);
@@ -21,6 +38,15 @@ namespace UsuariosAPI.Services
             EnviarEmailMime(email);
         }
 
+        /// <summary>
+        ///     Envia um e-mail para o destinatário informados.
+        /// </summary>
+        /// <remarks>
+        ///     Utiliza o remetente padrão da API para enviar o e-mail.
+        /// </remarks>
+        /// <param name="destinatario">Endereço de e-mail ao qual a mensagem será enviada.</param>
+        /// <param name="assunto">Assunto da mensagem de e-mail.</param>
+        /// <param name="corpoEmail">Corpo do texto do e-mail.</param>
         public void EnviarEmail(string destinatario, string assunto, string corpoEmail)
         {
             var email = new Email(destinatario, assunto, corpoEmail);
@@ -28,6 +54,10 @@ namespace UsuariosAPI.Services
             EnviarEmailMime(email);
         }
 
+        /// <summary>
+        ///     Envia um e-mail com o auxílio do MimeKit
+        /// </summary>
+        /// <param name="email">Uma classe <see cref="Email"/></param>
         private void EnviarEmailMime(Email email)
         {
             var mimeMessage = ToMimeMessage(email);
@@ -59,7 +89,15 @@ namespace UsuariosAPI.Services
             }
         }
 
-        public MimeMessage ToMimeMessage(Email email)
+        /// <summary>
+        ///     Converte uma classe <see cref="Email"/> para uma
+        ///     <see cref="MimeMessage"/> do MimeKit.
+        /// </summary>
+        /// <param name="email">A classe de e-mail.</param>
+        /// <returns>
+        ///     A classe <see cref="Email"/> convertida para <see cref="MimeMessage"/>.
+        /// </returns>
+        private MimeMessage ToMimeMessage(Email email)
         {
             MimeMessage mimeMessage = new MimeMessage();
 
@@ -70,7 +108,9 @@ namespace UsuariosAPI.Services
             mimeMessage.From.Add(fromAddress);
 
             List<MailboxAddress> destinatarios = new List<MailboxAddress>();
-            destinatarios.AddRange(email.Destinatarios.Select(d => new MailboxAddress("", d)));
+            destinatarios.AddRange(email
+                .Destinatarios
+                .Select(d => new MailboxAddress("", d)));
             mimeMessage.To.AddRange(destinatarios);
 
             mimeMessage.Subject = email.Assunto;
