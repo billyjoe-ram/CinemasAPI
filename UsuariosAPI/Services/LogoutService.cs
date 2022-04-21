b8fc69edd962ebd0e3ae1909c9e2c39f05f74a72
Billy Joe <billyjoersantos@outlook.com>
2022-04-20 22:33:21 -0300

    Documentando service de Logout

--- UsuariosAPI/Services/LogoutService.cs
+++ UsuariosAPI/Services/LogoutService.cs
@@ -4,17 +4,31 @@ using Microsoft.AspNetCore.Identity;
 
 namespace UsuariosAPI.Services
 {
+    /// <summary>
+    ///     Service para as operações e regras de negócio relacionado ao logout.
+    /// </summary>
     public class LogoutService
     {
         private SignInManager<IdentityUser<int>> _signInManager;
         private TokenService _tokenService;
 
+        /// <summary>
+        ///     Inicia uma nova instância da classe <see cref="LogoutService"/>
+        /// </summary>
+        /// <param name="signInManager"></param>
+        /// <param name="tokenService"></param>
         public LogoutService(SignInManager<IdentityUser<int>> signInManager, TokenService tokenService)
         {
             _tokenService = tokenService;
             _signInManager = signInManager;
         }
 
+        /// <summary>
+        ///     Desconecta um usuário do sistema.
+        /// </summary>
+        /// <exception cref="LogoutException">
+        ///     Lançada quando não é possível realizar o logout.
+        /// </exception>
         public void LogoutUsuario()
         {
             var resultadoIdentity = _signInManager.SignOutAsync();
