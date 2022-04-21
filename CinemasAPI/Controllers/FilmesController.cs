b9bf960a146d72a2247637cf96532699c41f4770
Billy Joe <billyjoersantos@outlook.com>
2022-04-21 09:19:18 -0300

    refactor(acessores/classes): CinemasAPI - Controller de Filmes
    Modifica o acessor de public para internal na classe FilmesController
    
    Relacionado à Issue #1

--- CinemasAPI/Controllers/FilmesController.cs
+++ CinemasAPI/Controllers/FilmesController.cs
@@ -12,7 +12,7 @@ namespace CinemasAPI.Controllers
     /// </summary>
     [ApiController]
     [Route("[controller]")]
-    public class FilmesController : ControllerBase
+    internal class FilmesController : ControllerBase
     {
         private FilmesService _filmesService;
 
